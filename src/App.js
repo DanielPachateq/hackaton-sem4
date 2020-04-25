4d9c3c2773bf6d6095f63cec95b8ab8eb0fe9c4d
dteranm <dteranm@gmail.com>
2020-04-25 14:37:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Formulario from './components/Formulario';
+import Registro from './components/Registro';
 
 function App() {
+
+  // Registros en local storage
+  let registrosIniciales = JSON.parse(localStorage.getItem('registros'));
+  if(!registrosIniciales) {
+    registrosIniciales = [];
+  }
+
+  // Arreglo de registros
+  const [registros, guardarRegistros] = useState(registrosIniciales);
+
+  // Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect( () => {
+      let registrosIniciales = JSON.parse(localStorage.getItem('registros'));
+
+      if(registrosIniciales) {
+        localStorage.setItem('registros', JSON.stringify(registros))
+      } else {
+        localStorage.setItem('registros', JSON.stringify([]));
+      }
+  }, [registros] );
+
+  // Función que toma los registros actuales y agregue una nueva
+  const crearRegistro = registro => {
+    guardarRegistros([ ...registros, registro ]);
+  }
+
+  // Función que elimina un registro por su id
+  const eliminarRegistro = id => {
+     const nuevosRegistros = registros.filter(registro => registro.id !== id );
+     guardarRegistros(nuevosRegistros);
+  }
+
+  // Mensaje condicional
+  const titulo = registros.length === 0 ? '' : 'Gracias por registrar tus datos';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Administrador de Datos</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+              <Formulario 
+                crearRegistro={crearRegistro}
+              />
+          </div>
+          <div className="one-half column">
+              <h2>{titulo}</h2>
+              {registros.map(registro => (
+                <Registro
+                  key={registro.id}
+                  registro={registro}
+                  eliminarRegistro={eliminarRegistro}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
